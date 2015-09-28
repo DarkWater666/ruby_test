8205a0fadd552276aa1e34eef3db7f4a2e6b075a
Denis Arushanov <denis.arushanov@saltpepper.ru>
2015-09-28 16:48:24 +0300

    Refactor task7

--- task7.rb
+++ task7.rb
@@ -1,29 +1,25 @@
 class Numeric
   @@default_curr = 'rouble'
-  @@in_roubles   = { @@default_curr => 1.0, 'dollar' => 67.95, 'euro' => 75.65 }
+  @@in_roubles   = { @@default_curr => 1.0, 'dollar' => 67.95, 'euro' => 75.65 } # currency exchange values on 27.09.2015
 
   @@in_roubles.keys.each do |curr|
     curr = curr.to_s.gsub(/s$/, '')
     define_method(curr) do
       @@current_curr = curr
-      self * @@in_roubles[@@current_curr]
+      return self
     end
     alias_method curr + 's', curr
   end
 
   def in(arg)
     curr = arg.to_s.gsub(/s$/, '')
-    @@in_roubles.key?(curr) ? exchange(curr) : close
+    @@in_roubles.key?(curr) ? exchange(self, curr) : close
   end
 
   private
 
-  def exchange(curr)
-    if @current_curr == curr || @@current_curr == @@default_curr || curr != @@default_curr
-      (self / @@in_roubles[curr]).round 2
-    else
-      self * @@in_roubles[curr]
-    end
+  def exchange(num, curr)
+    ( num * ( @@in_roubles[@@current_curr] / @@in_roubles[curr] )).round 2
   end
 
   def close
